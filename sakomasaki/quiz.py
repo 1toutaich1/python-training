ee1a3e507b8b869c501441c685397f21a4857aaf
MasakiSako <sako.masaki.jt@mynavi.jp>
2024-05-14 06:31:49 +0000

    -sal

--- sakomasaki/quiz.py
+++ sakomasaki/quiz.py
@@ -1,7 +1,11 @@
-import sys
-args = sys.argv
-num = int(args[1])
-if num%2==0 :
-    print("偶数")
-else:
-    print("奇数")
+user = input("type jikyuu:")
+jikyu = int(user)
+
+user = input("how long:")
+jikan = int(user)
+
+kyuryou=jikyu*jikan
+
+fmt= """時給{0}円で{1}時間働いたので給料は{2}円です."""
+desc=fmt.format(jikyu,jikan,kyuryou)
+print(desc)
