03ddff26c3654dae75ff0a47044f06f555b5811d
1toutaich1 <itou.taichi.jd@mynavi.jp>
2024-05-14 05:20:57 +0000

    add q2

--- ito-taichi/mynameout.py
+++ ito-taichi/mynameout.py
@@ -1,1 +1,7 @@
-print('Hello Ito Taichi !')
+import sys
+args = sys.argv
+
+name = args[1]
+
+print(f"Hello {name} !",end="")
+# print("Hello " + name + " !" ,end="")
---
+++ ito-taichi/q2.py
@@ -0,0 +1,7 @@
+import sys
+args = sys.argv
+
+a = int(args[1])
+b = int(args[2])
+c = int(args[3])
+print(a + b + c,end="")
