7e58989cc4b6264698f66f65bbb60a6cab652961
MasakiSako <sako.masaki.jt@mynavi.jp>
2024-05-15 03:20:00 +0000

    -

--- sakomasaki/quiz.py
+++ sakomasaki/quiz.py
@@ -1,11 +1,6 @@
 import sys
-from decimal import Decimal,ROUND_HALF_UP
+#from decimal import Decimal,ROUND_HALF_UP
 args = sys.argv
 a = int(args[1])
-b = int(args[2])
-c = int(args[3])
-
-if (a>=50 and b>=50 and c>=50) and ((a>=70 and b>=70 and c>=70) or (a+b+c)>220):
-    print("合格です")
-else:
-    print("不合格です")
+for i in range(1,a+1):
+    print("ヒツジが"+str(i)+"匹")
