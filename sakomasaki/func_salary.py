c075f5dcd1f63a12f5c64ebe4b59d7ec49ffd03b
MasakiSako <167750438+MasakiSako@users.noreply.github.com>
2024-05-17 16:00:09 +0900

    add salary

---
+++ sakomasaki/func_salary.py
@@ -0,0 +1,8 @@
+from decimal import Decimal,ROUND_HALF_UP
+def calcsalary(sal):
+    if sal>=1e6:
+        tax=0.2*sal-1e5
+    else:
+        tax=0.1*sal
+    tax= Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+    return (sal-tax),tax
