fa881789df42999737c28527647f7a3aefb2b322
MasakiSako <sako.masaki.jt@mynavi.jp>
2024-05-21 06:01:35 +0000

    a

--- sakomasaki/c3.py
+++ sakomasaki/c3.py
@@ -5,7 +5,7 @@ from database import session
 from c3tables import *
 from sqlalchemy.sql import func
 
-#
+##
 #result = session.query(Mst_merchandise).filter(Mst_merchandiseTbl_stock.stock!=0).all()
 
 print(result)
